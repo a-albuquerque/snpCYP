ae9773a86506364d7ec3582e2650dca9d044eeb6
Alcides Albuquerque <a.albuquerque@mail.utoronto.ca>
2021-11-16 12:42:01 -0300

    Graphic, debug

--- R/detectSNP.R
+++ R/detectSNP.R
@@ -12,6 +12,7 @@
 #' @param seqs Aminoacid sequences in FASTA format, list of strings
 #'
 #' @returns Visualization of the amount of snSNPs for each CYP analyzed
+#'          Also return the SNPs positions for each CYP
 #'
 #' @examples
 #' \dontrun{
@@ -43,6 +44,7 @@
 detectSNP <- function(CYPs, seqs) {
 
   library(assertthat)
+  library(graphics)
 
   if (!is.string(CYPs[[1]])){
     stop("CYP isoform must be provided as non empty list of strings")
@@ -55,6 +57,8 @@ detectSNP <- function(CYPs, seqs) {
   load(file = "./data/baseSeqs.rda")
 
   counts <- vector()
+  positions <- vector()
+
 
   for (i in 1:length(CYPs)) {
 
@@ -64,26 +68,26 @@ detectSNP <- function(CYPs, seqs) {
     wildForm <- baseSeqs[CYPs[[i]]]
     wildFormVector <- unlist(strsplit(wildForm, ""))
 
+
+
     count <- 0
 
     for (j in 1:length(wildFormVector)) {
-      #print(wildFormVector[[j]])
-      #print(seqVector[j])
+
       if (wildFormVector[[j]] != seqVector[[j]]) {
         count = count + 1
+        positions <- append(positions, c(CYPs[[i]], toString(j)))
       }
     }
 
     counts <- append(counts, count)
 
 
-    #print(CYPs[[i]])
-    #print(wildFormVector)
-    #print(seqVector)
-
   }
 
+  barplot(counts, xlab="CYP isoform", ylab="Number of nsSNP",
+          main="Distribution of nsSNP across sample isoforms", names.arg=CYPs, col="green")
 
-  return (counts)
+  return (positions)
 
 }
